d93094494ef8881968d68678890c7b6b5585bb4f
Udaky <rizkyfakhri227@gmail.com>
2023-12-31 19:14:57 +0700

    changes

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1997479a-0a60-4c48-a027-e9d260193d49';
+export const MODAL_CLOSE_SEC = 2.5;
